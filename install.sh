00fb2341c9e2b85b0ab8f581da9712302e3c34d0
Michael T. DeGuzis <mdeguzis@users.noreply.github.com>
2024-05-29 11:14:04 -0400

    syntax issue

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ printf "\nInstalling cloud sync services\n"
 if [[ ! -d "/opt/arklone" ]]; then
     git clone --depth 1 https://github.com/ridgekuhn/arklone-arkos /opt/arklone
 else
-    git -C /opt/arklone --reset hard
+    git -C /opt/arklone reset --hard
     git -C /opt/arklone pull --rebase
 fi
 sudo chown ark:ark /opt/arklone
